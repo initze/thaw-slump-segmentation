fixed DEM visual output in visual output plots fixed erroneous scaling of DEM values

deep_learning/utils/plot_info.py
@@ -14,6 +14,7 @@ def showexample(batch, preds, idx, filename):
     fig, ax = plt.subplots(ROWS, N, figsize=(3*N, 3*ROWS), gridspec_kw=gridspec_kw)
     ax = ax.T.reshape(-1)
     heatmap_args = dict(cmap='coolwarm', vmin=0, vmax=1)
+    heatmap_dem = dict(cmap='RdBu', vmin=0, vmax=0.9)
 
     batch_img, batch_target = batch
     batch_img = batch_img.to(torch.float)
@@ -26,9 +27,10 @@ def showexample(batch, preds, idx, filename):
     rgb = batch_img[idx, [3, 2, 1]].cpu().numpy()
     ndvi = batch_img[idx, [4, 4, 4]].cpu().numpy()
     tcvis = batch_img[idx, [5, 6, 7]].cpu().numpy()
-    dem = batch_img[idx, [8, 8, 9]].cpu().numpy()
-    dem[0] = -dem[0] # Red is negative, green is positive
-    dem *= np.array([500, 500, 2]).reshape(-1, 1, 1)
+    dem = batch_img[idx, [8, 8, 8]].cpu().numpy()
+    #dem = batch_img[idx, [8, 8, 9]].cpu().numpy()
+    #dem[0] = -dem[0] # Red is negative, green is positive
+    #dem *= np.array([500, 500, 2]).reshape(-1, 1, 1)
 
     ax[0].imshow(np.clip(rgb.transpose(1, 2, 0), 0, 1))
     ax[0].set_title('B-G-NIR')
@@ -36,7 +38,7 @@ def showexample(batch, preds, idx, filename):
     ax[1].set_title('NDVI')
     ax[2].imshow(np.clip(tcvis.transpose(1, 2, 0), 0, 1))
     ax[2].set_title('TCVis')
-    ax[3].imshow(np.clip(dem.transpose(1, 2, 0), 0, 1))
+    ax[3].imshow(np.clip(dem[0], 0, 1), **heatmap_dem)
     ax[3].set_title('DEM')
     ax[4].imshow(batch_target[idx, 0].cpu(), **heatmap_args)
     ax[4].set_title('Target')
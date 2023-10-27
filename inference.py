09a634139e621484f4bcf97c0d2e0387d37749b6
Konrad Heidler <k.heidler@tum.de>
2023-10-27 15:49:00 +0200

    Inference for datacubes, still needs minor adjustments

--- inference.py
+++ inference.py
@@ -13,47 +13,27 @@ import argparse
 from pathlib import Path
 
 import rasterio as rio
+import rioxarray
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 from datetime import datetime
+import geopandas as gpd
 
 from lib.models import create_model
-from lib.utils.plot_info import flatui_cmap
-from lib.utils import init_logging, get_logger, log_run
-from lib.data_pre_processing import gdal
-
-from setup_raw_data import preprocess_directory
-from data_loading import DataSources
+from lib.utils import init_logging, get_logger, extract_contours, Compositor
+from lib.data.loading import NCDataset
+import cv2
 
 import yaml
 
-cmap_prob = flatui_cmap('Midnight Blue', 'Alizarin')
-cmap_dem = flatui_cmap('Alizarin', 'Clouds', 'Peter River')
-cmap_slope = flatui_cmap('Clouds', 'Midnight Blue')
 cmap_ndvi = 'RdYlGn'
 
 FIGSIZE_MAX = 20
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--gdal_bin", default='', help="Path to gdal binaries")
-parser.add_argument("--gdal_path", default='', help="Path to gdal scripts")
-parser.add_argument("--n_jobs", default=-1, type=int, help="number of parallel joblib jobs")
-parser.add_argument("--ckpt", default='latest', type=str, help="Checkpoint to use")
-parser.add_argument("--data_dir", default='data', type=Path, help="Path to data processing dir")
-parser.add_argument("--log_dir", default='logs', type=Path, help="Path to log dir")
-parser.add_argument("--inference_dir", default='inference', type=Path, help="Main inference directory")
-parser.add_argument("-n", "--name", default=None, type=str, help="Name of inference run, data will be stored in subdirectory")
-parser.add_argument("-m", "--margin_size", default=256, type=int, help="Size of patch overlap")
-parser.add_argument("-p", "--patch_size", default=1024, type=int, help="Size of patches")
-parser.add_argument("model_path", type=str, help="path to model")
-parser.add_argument("tile_to_predict", type=str, help="path to model", nargs='+')
-
-args = parser.parse_args()
-gdal.initialize(args)
 
 
 def predict(model, imagery, device='cpu'):
@@ -99,154 +79,86 @@ def flush_rio(filepath):
         pass
 
 
-def do_inference(tilename, args=None, log_path=None):
+def do_inference(tile_path, model, args, config, dev, log_path=None):
+    model = model.to(dev)
     tile_logger = get_logger(f'inference.{tilename}')
     # ===== PREPARE THE DATA =====
     DATA_ROOT = args.data_dir
     INFERENCE_ROOT = args.inference_dir
     data_directory = DATA_ROOT / 'tiles' / tilename
-    if not data_directory.exists():
-        logger.info(f'Preprocessing directory {tilename}')
-        raw_directory = DATA_ROOT / 'input' / tilename
-        if not raw_directory.exists():
-            logger.error(f"Couldn't find tile '{tilename}' in {DIR_ROOT}/tiles or {DIR_ROOT}/input. Skipping this tile")
-            return
-        preprocess_directory(raw_directory, args, log_path, label_required=False)
-        # After this, data_directory should contain all the stuff that we need.
-    
+
     if args.name:
         output_directory = INFERENCE_ROOT / args.name / tilename
     else:
         output_directory = INFERENCE_ROOT / tilename
-    
+
     output_directory.mkdir(exist_ok=True, parents=True)
 
-    planet_imagery_path = next(data_directory.glob('*_SR.tif'))
+    # TODO: Overlap mode
+    data = NCDataset(tile_path, config)
+    loader = DataLoader(data, batch_size=config['batch_size'], num_workers=1, pin_memory=True)
 
-    data = []
-    for source in sources:
-        tile_logger.debug(f'loading {source.name}')
-        if source.name == 'planet':
-            tif_path = planet_imagery_path
-        else:
-            tif_path = data_directory / f'{source.name}.tif'
+    result = Compositor()
+    nodata = Compositor()
+    for batch in loader:
+      img = batch[0].to(dev)
+      nds = (batch[0].numpy() == 0).all(axis=1)
+      metadata = batch[-1]
+      preds = torch.sigmoid(model(img))[:, 0]
+      preds = preds.cpu().numpy()
+      for pred, nd, y, x in zip(preds, nds, metadata['y0'], metadata['x0']):
+        result.add_tile((y, x), pred)
+        nodata.add_tile((y, x), nd)
 
-        data_part = rio.open(tif_path).read().astype(np.float32)
+    result = result.compose()
+    nodata = nodata.compose() > 0.5
 
-        if source.name == 'tcvis':
-            data_part = data_part[:3]
-        data_part = np.nan_to_num(data_part, nan=0.0)
+    input_nc = rioxarray.open_rasterio(tile_path)
+    profile = {
+        'driver': 'GTiff',
+        'dtype': np.float32,
+        'width': result.shape[1],
+        'height': result.shape[0],
+        'count': 1,
+        'transform': input_nc.rio.transform(),
+        'crs': input_nc.rio.crs,
+        'compress': 'LZW',
+    }
 
-        data_part = data_part / np.array(source.normalization_factors, dtype=np.float32).reshape(-1, 1, 1)
-        data.append(data_part)
+    print(profile)
+    result[nodata] = np.nan
+    with rio.open(output_directory / 'pred_probability.tif', 'w', **profile) as out:
+      out.write(result, 1)
 
-    def make_img(filename, source, colorbar=False, mask=None, **kwargs):
-        idx = sources.index(source)
-        h, w = data[idx].shape[1:]
-        if mask is None:
-            mask = np.zeros((h, w), dtype=np.bool)
-        if h > w:
-            figsize = (FIGSIZE_MAX * w / h, FIGSIZE_MAX)
-        else:
-            figsize = (FIGSIZE_MAX, FIGSIZE_MAX * h / w)
-        fig, ax = plt.subplots(figsize=figsize)
-        ax.axis('off')
+    contours = extract_contours(result, 0.5)
+    contours = gpd.GeoSeries(contours, crs=input_nc.rio.crs)
 
-        if source.channels >= 3:
-            rgb = np.stack([np.ma.masked_where(mask, data[idx][i])[::10, ::10] for i in range(3)], axis=-1)
-            rgb = np.clip(rgb, 0, 1)
-            ax.imshow(rgb, aspect='equal')
-        elif source.channels == 1:
-            image = ax.imshow(np.ma.masked_where(mask, data[idx][0]), aspect='equal', **kwargs)
-            if colorbar:
-                plt.colorbar(image)
-        plt.savefig(output_directory / filename, bbox_inches='tight', pad_inches=0)
-        plt.close()
+    tx = input_nc.rio.transform()
+    mat = [tx.a, tx.b, tx.d, tx.e, tx.c, tx.f]
+    contours = contours.buffer(0)
+    contours = contours.affine_transform(mat)
+    contours.to_file(output_directory / 'predictions.shp')
 
-    def plot_results(image, outfile):
-        fig, ax = plt.subplots(figsize=figsize)
-        ax.axis('off')
-        mappable = ax.imshow(image, vmin=0, vmax=1, cmap=cmap_prob, aspect='equal')
-        plt.colorbar(mappable)
-        plt.savefig(outfile, bbox_inches='tight', pad_inches=0)
-        plt.close()
-
-
-    full_data = np.concatenate(data, axis=0)
-    nodata = np.all(full_data == 0, axis=0, keepdims=True)
-    full_data = torch.from_numpy(full_data)
-    full_data = full_data.unsqueeze(0)  # Pretend this is a batch of size 1
-
-    res = predict(model, full_data, dev).numpy()
-    del full_data
-
-    res[nodata] = np.nan
-    binarized = np.ones_like(res, dtype=np.uint8) * 255
-    binarized[~nodata] = (res[~nodata] > 0.5).astype(np.uint8)
-
-    # define output file paths
-    out_path_proba = output_directory / 'pred_probability.tif'
-    out_path_label = output_directory / 'pred_binarized.tif'
-    out_path_pre_poly = output_directory / 'pred_binarized_tmp.tif'
-    out_path_shp = output_directory / 'pred_binarized.shp'
-
-    # Get the input profile
-    with rio.open(planet_imagery_path) as input_raster:
-        profile = input_raster.profile
-        profile.update(
-            dtype=rio.float32,
-            count=1,
-            compress='lzw'
-        )
-
-    with rio.open(out_path_proba, 'w', **profile) as output_raster:
-        output_raster.write(res.astype(np.float32))
-    flush_rio(out_path_proba)
-
-    profile.update(
-        dtype=rio.uint8,
-        nodata=255
-    )
-    with rio.open(out_path_label, 'w', **profile) as output_raster:
-        output_raster.write(binarized)
-    flush_rio(out_path_label)
-
-    with rio.open(out_path_pre_poly, 'w', **profile) as output_raster:
-        output_raster.write((binarized == 1).astype(np.uint8))
-    flush_rio(out_path_pre_poly)
-
-    # create vectors
-    log_run(f'{gdal.polygonize} {out_path_pre_poly} -q -mask {out_path_pre_poly} -f "ESRI Shapefile" {out_path_shp}', tile_logger)
-    #log_run(f'python {gdal.polygonize} {out_path_pre_poly} -q -mask {out_path_pre_poly} -f "ESRI Shapefile" {out_path_shp}', tile_logger)
-    out_path_pre_poly.unlink()
-
-    h, w = res.shape[1:]
-    if h > w:
-        figsize = (FIGSIZE_MAX * w / h, FIGSIZE_MAX)
-    else:
-        figsize = (FIGSIZE_MAX, FIGSIZE_MAX * h / w)
-
-    for src in sources:
-        kwargs = dict()
-        if src.name == 'ndvi':
-            kwargs = dict(colorbar=True, cmap=cmap_ndvi, vmin=0, vmax=1)
-        elif src.name == 'relative_elevation':
-            kwargs = dict(colorbar=True, cmap=cmap_dem, vmin=0, vmax=1)
-        elif src.name == 'slope':
-            kwargs = dict(colorbar=True, cmap=cmap_slope, vmin=0, vmax=0.5)
-        make_img(f'{src.name}.jpg', src, mask=nodata[0],**kwargs)
-
-    outpath = output_directory / 'pred_probability.jpg'
-    plot_results(np.ma.masked_where(nodata[0], res[0]), outpath)
-
-    outpath = output_directory / 'pred_binarized.jpg'
-    plot_results(np.ma.masked_where(nodata[0], binarized[0]), outpath)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_jobs", default=-1, type=int, help="number of parallel joblib jobs")
+    parser.add_argument("--ckpt", default='latest', type=str, help="Checkpoint to use")
+    parser.add_argument("--data_dir", default='data', type=Path, help="Path to data processing dir")
+    parser.add_argument("--log_dir", default='logs', type=Path, help="Path to log dir")
+    parser.add_argument("--inference_dir", default='inference', type=Path, help="Main inference directory")
+    parser.add_argument("-n", "--name", default=None, type=str, help="Name of inference run, data will be stored in subdirectory")
+    parser.add_argument("-m", "--margin_size", default=256, type=int, help="Size of patch overlap")
+    parser.add_argument("-p", "--patch_size", default=1024, type=int, help="Size of patches")
+    parser.add_argument("model_path", type=str, help="path to model")
+    parser.add_argument("tile_to_predict", type=str, help="path to model", nargs='+')
+
+    args = parser.parse_args()
+
     timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     log_path = Path(args.log_dir) / f'inference-{timestamp}.log'
     if not Path(args.log_dir).exists():
-	    os.mkdir(Path(args.log_dir))
+        os.mkdir(Path(args.log_dir))
     init_logging(log_path)
     logger = get_logger('inference')
 
@@ -268,10 +180,9 @@ if __name__ == "__main__":
 
     model_dir = Path(args.model_path)
     model_name = model_dir.name
-    config = yaml.load((model_dir / 'config.yml').open(), Loader=yaml.SafeLoader)
+    config = yaml.load((model_dir / 'config.yml').open(), Loader=yaml.Loader)
 
     m = config['model']
-    #print(m['architecture'],m['encoder'], m['input_channels'])
     model = create_model(
         arch=m['architecture'],
         encoder_name=m['encoder'],
@@ -287,19 +198,15 @@ if __name__ == "__main__":
         last_ckpt = int(args.ckpt)
     ckpt = model_dir / 'checkpoints' / f'{last_ckpt:02d}.pt'
     logger.info(f"Loading checkpoint {ckpt}")
-    
+
     # Parallelized Model needs to be declared before loading
     try:
         model.load_state_dict(torch.load(ckpt, map_location=dev))
     except:
         model = nn.DataParallel(model)
         model.load_state_dict(torch.load(ckpt, map_location=dev))
-    
-    model = model.to(dev)
-
-    sources = DataSources(config['data_sources'])
 
     torch.set_grad_enabled(False)
 
     for tilename in tqdm(args.tile_to_predict):
-        do_inference(tilename, args, log_path)
+        do_inference(tilename, model, args, config, dev, log_path)
--- lib/utils/images.py
+++ lib/utils/images.py
@@ -4,6 +4,11 @@
 # LICENSE file in the root directory of this source tree.
 
 import numpy as np
+from itertools import zip_longest
+from functools import reduce, lru_cache
+import cv2
+import shapely as shp
+from collections import defaultdict
 
 
 def extract_patches(images, patch_size=512, max_nodata=0.0, stride=None):
@@ -31,3 +36,80 @@ def extract_patches(images, patch_size=512, max_nodata=0.0, stride=None):
                 nodata = nodata.all(axis=0)
             if nodata.mean() <= max_nodata:
                 yield [x_i, y_i, *cutouts]
+
+
+
+@lru_cache(16)
+def make_stencil(dims):
+  dims = dims[:-1]
+
+  parts = []
+  for i, dim in enumerate(dims):
+    l1 = dim // 2
+    l2 = dim - l1
+    part = np.concatenate([np.linspace(0.1, 1, l1), np.linspace(1, 0.1, l2)])
+    for j in range(len(dims)):
+      if j != i:
+        part = np.expand_dims(part, j)
+    parts.append(part)
+
+  return reduce(np.minimum, parts)[..., np.newaxis]
+
+
+
+class Compositor:
+  tiles: dict
+  max_coords: list[int]
+
+  def __init__(self):
+    self.tiles = {}
+    self.max_coords = []
+
+  def add_tile(self, coords: tuple[int, ...], tile: np.ndarray):
+    extent = [coord + shp for coord, shp in zip(coords, tile.shape)]
+    self.max_coords = [max(c, e) for c, e in
+                       zip_longest(self.max_coords, extent, fillvalue=0)]
+    self.tiles[coords] = tile
+
+  def compose(self):
+    assert self.max_coords is not None
+    wgt = np.zeros([*self.max_coords], dtype=np.float64)
+    out = np.zeros([*self.max_coords], dtype=np.float64)
+
+    for coords, tile in self.tiles.items():
+      stencil = make_stencil(tile.shape)
+      slices = tuple(slice(c, c+e) for c, e in zip(coords, tile.shape))
+      # We could just write out[*slices], but only starting from Py3.11 :)
+      out.__setitem__(slices, out.__getitem__(slices) + stencil * tile)
+      wgt.__setitem__(slices, wgt.__getitem__(slices) + stencil)
+    wgt = np.where(wgt == 0, 1, wgt)
+
+    composed = out / wgt
+    return composed
+
+
+
+def extract_contours(image, threshold):
+    mask = (image > threshold).astype(np.uint8)
+    contours, hierarchy = cv2.findContours(mask, mode=cv2.RETR_CCOMP, method=cv2.CHAIN_APPROX_TC89_KCOS)
+    # Match children and parents, cf. https://michhar.github.io/masks_to_polygons_and_back/
+    if not contours:
+      return []
+
+    assert hierarchy.shape[0] == 1
+    children = set()
+    cnt_children = defaultdict(list)
+    for idx, (_, _, _, parent_idx) in enumerate(hierarchy[0]):
+      if parent_idx != -1:
+        children.add(idx)
+        cnt_children[parent_idx].append(contours[idx])
+
+    # create actual polygons filtering by area (removes artifacts)
+    all_polygons = []
+    for idx, cnt in enumerate(contours):
+      if idx not in children and cv2.contourArea(cnt) >= 2.0:
+        assert cnt.shape[1] == 1
+        holes = [c[:, 0, :] for c in cnt_children.get(idx, []) if cv2.contourArea(c) >= 2.]
+        poly = shp.Polygon(shell=cnt[:, 0, :], holes=holes)
+        all_polygons.append(poly)
+    return all_polygons

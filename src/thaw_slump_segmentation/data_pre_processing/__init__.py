b8fc3eb129e3deaf8a9dfeed3e4b39b0feabd3a2
Ingmar Nitze <ingmarnitze@gmail.com>
2024-07-18 15:40:59 +0200

    import fix preprocessing

--- src/thaw_slump_segmentation/data_pre_processing/__init__.py
+++ src/thaw_slump_segmentation/data_pre_processing/__init__.py
@@ -4,7 +4,7 @@
 # LICENSE file in the root directory of this source tree.
 
 from . import utils, earthengine, gdal, udm
-from .utils import *
-from .earthengine import *
+from utils import *
+from earthengine import *
 
 _logger = get_logger('preprocessing.ee')
